1c5550d109e18d7eee0eb3469637beda302e29ea
wzrdk3lly <dkmk1149@gmail.com>
2022-06-03 17:49:13 -0400

    created outline of logic

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hellow world")
+// TODO1 -  make numbers appear in input section/ style it
+// TODO2 -  
+
+// remaining todos...
+// We will need functions for all the computation types (+, -, x, /)
+
+// They should probably take in 2 operands and then perform calculation
+// ie function add (operand1,operand2=operand1) 
+// *made operand 2 equate to operand 1 by default 
+// because that's what the apple calculator does
+
+// we will likely need to make the operand 1 and operand 2 global variables to keep state
+
+// after each operator is is pressed store the result as the global operand in order to keep "state"
